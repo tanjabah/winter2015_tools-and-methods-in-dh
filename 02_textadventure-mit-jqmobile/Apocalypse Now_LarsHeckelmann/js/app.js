4ee424a1d329d4a039e85391c242836d0d3ac582
MatthiasNRW <matthiasschaar@vodafone.de>
2016-02-14 20:08:38 +0100

    Apocalypse Now  by Lars Heckelmann

---
+++ 02_textadventure-mit-jqmobile/Apocalypse Now_LarsHeckelmann/js/app.js
@@ -0,0 +1,97 @@
+$(document).ready(function() { 
+
+$("#steuern_intro").click(function(){
+    $("#intro").toggle();
+    $("#opt3").toggle();
+});
+
+$("#steuern_intro2").click(function(){
+    $("#intro").toggle();
+    $("#opt4").toggle();
+});
+
+$("#steuern_intro3").click(function(){
+    $("#intro").toggle();
+    $("#opt6").toggle();
+});
+
+$("#steuern_intro4").click(function(){
+    $("#intro").toggle();
+    $("#opt7").toggle();
+});
+
+$("#steuern_intro5").click(function(){
+    $("#intro").toggle();
+    $("#opt9").toggle();
+});
+
+$("#steuern_intro6").click(function(){
+    $("#intro").toggle();
+    $("#opt11").toggle();
+});
+
+$("#steuern_intro7").click(function(){
+    $("#intro").toggle();
+    $("#opt12").toggle();
+});
+
+$("#steuern_opt1").click(function(){
+    $("#opt1").toggle();
+    $("#intro").toggle();
+});
+
+$("#steuern_opt2").click(function(){
+    $("#opt2").toggle();
+    $("#intro").toggle();
+});
+
+$("#steuern_opt3").click(function(){
+    $("#opt3").toggle();
+    $("#opt1").toggle();
+});
+
+$("#steuern_opt4").click(function(){
+    $("#opt4").toggle();
+    $("#opt1").toggle();
+});
+
+$("#steuern_opt5").click(function(){
+    $("#opt5").toggle();
+    $("#opt2").toggle();
+});
+
+$("#steuern_opt6").click(function(){
+    $("#opt6").toggle();
+    $("#opt2").toggle();
+});
+
+$("#steuern_opt7").click(function(){
+    $("#opt7").toggle();
+    $("#opt5").toggle();
+});
+
+$("#steuern_opt8").click(function(){
+    $("#opt8").toggle();
+    $("#opt5").toggle();
+});
+
+$("#steuern_opt9").click(function(){
+    $("#opt9").toggle();
+    $("#opt8").toggle();
+});
+
+$("#steuern_opt10").click(function(){
+    $("#opt10").toggle();
+    $("#opt8").toggle();
+});
+
+$("#steuern_opt11").click(function(){
+    $("#opt11").toggle();
+    $("#opt10").toggle();
+});
+
+$("#steuern_opt12").click(function(){
+    $("#opt12").toggle();
+    $("#opt10").toggle();
+});
+});
